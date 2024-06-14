c973829ce6970d2661fb5cb3fab337094aeca144
Axel JARRY <axeljarry0@gmail.com>
2024-06-14 15:13:55 +0200

    Ajout page restaurant + route index > restaurant

---
+++ app/restaurant.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { firebase } from '@react-native-firebase/firestore';
+
+const Restaurant = () => {
+    return (
+        <Text>
+            Coucou
+        </Text>
+    )
+}
+
+export default Restaurant 
